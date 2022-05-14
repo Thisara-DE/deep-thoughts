b94c553a7aa0c6fd8cbf3e3d793affb8b0fced6f
Glorzo <thisaraeranga@gmail.com>
2022-05-14 01:59:29 -0500

    add authentication

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Header from './components/Header';
@@ -16,8 +17,18 @@ const httpLink = createHttpLink({
   uri: '/graphql'
 });
 
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ''
+    }
+  };
+})
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache()
 })
 
@@ -32,7 +43,7 @@ function App() {
               <Route path="/" element={<Home />} />
               <Route path="/login" element={<Login />} />
               <Route path="/signup" element={<Signup />} />
-              <Route path="/profile/:username" element={<Profile />} />
+              <Route path="/profile/:username?" element={<Profile />} />
               <Route path="/thought/:id" element={<SingleThought />} />
               <Route path='*' element={<NoMatch />} />
             </Routes>
--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,7 +1,11 @@
 import React, { useState } from 'react';
+import {  useMutation } from '@apollo/client';
+import { ADD_USER } from '../utils/mutations';
+import auth from '../utils/auth';
 
 const Signup = () => {
   const [formState, setFormState] = useState({ username: '', email: '', password: '' });
+  const [addUser, { error }] = useMutation(ADD_USER);
 
   // update state based on form input changes
   const handleChange = (event) => {
@@ -16,6 +20,19 @@ const Signup = () => {
   // submit form
   const handleFormSubmit = async (event) => {
     event.preventDefault();
+
+    // use try/catch instead of promises to handle errors]
+    try{
+      // execure adduser mutation and pass in variables data from form
+      const { data } = await addUser({
+        variables: { ...formState }
+      });
+      
+      auth.login(data.addUser.token);
+    }
+    catch (e) {
+      console.error(e)
+    }
   };
 
   return (
@@ -56,6 +73,7 @@ const Signup = () => {
                 Submit
               </button>
             </form>
+            {error && <div>Sign up failed</div>}            
           </div>
         </div>
       </div>
